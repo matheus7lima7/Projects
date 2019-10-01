696168fda554c8d5c9cc2892e4a96ebedb5d29e9
matheus7lima7 <55416604+matheus7lima7@users.noreply.github.com>
2019-10-01 18:49:14 -0300

    Add files via upload

---
+++ números iguais e não iguais.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int n1, n2;
+	printf("Digite um Numero:     ");
+	scanf("%d", &n1);
+	printf("Digite outro Numero:     ");
+	scanf("%d", &n2);
+	if (n1==n2){
+		printf("sim");
+		
+	}
+	else {
+		printf("nao");
+	}
+}
